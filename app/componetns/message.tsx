4efb83cc783906dd80299c7278a986aad610a2a9
seito-1 <reline-seito1@kidsdoor.net>
2026-02-12 18:59:08 +0900

    first commit

---
+++ app/componetns/message.tsx
@@ -0,0 +1,11 @@
+export const Message = () => {
+    return (
+        <>
+            <div className = "bg-red-900 w-32 p-4 m-8 rounded-md shadow-xl relative">
+                <p className = "text-white">test</p>
+                <p className = "text-xs absolute -left-8 -bottom-6">2026-2-12</p>
+            </div>
+        </>
+        
+    )
+}
